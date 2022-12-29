8c574e7c80c4733aa521c697cf314866360f9c16
Shubham Sharma <shubhambabai8@gmail.com>
2022-12-29 13:24:33 +0530

    binary to decimal

--- converter.cpp
+++ converter.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main(){
-    int n;
-    cout << "Enter a number : ";
-    cin<<n;
-    while(n/2){
-        cout << n%2 << endl;
-        n = n/2;
+    int n, decimal = 0, pow = 1;
+    cout << "Enter a binary number : ";
+    cin>>n;
+    while(n){
+        int last_bit = n%10;
+        decimal = decimal + last_bit*pow;
+        n = n/10;
+        pow *= 2;
     }
+
+    cout << "Here's your result look at this : " << decimal << endl; 
     return 0;
 }
